af79f8596edd00109d70e1ee7674415ee45c2b8a
vincentmylee <vincentmylee@gmail.com>
2019-09-16 17:38:01 +0800

    add Optional code

--- src/main/java/com/rp/lib/optional/OptionalExample.java
+++ src/main/java/com/rp/lib/optional/OptionalExample.java
@@ -1,4 +1,21 @@
 package com.rp.lib.optional;
 
+import java.util.Optional;
 public class OptionalExample {
+
+
+    public static void main(String[] args) {
+        optionalFilter();
+    }
+
+    static void optionalFilter(){
+        String value = "";
+        String result = Optional.ofNullable(value)
+                .filter(e -> e.equals("18") || e.equals("19"))
+                .map(v -> "Y")
+                .orElse("N");
+
+        System.out.println("reslut= "+result);
+    }
+
 }
